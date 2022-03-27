c2ed9f4f0b8b5f268ce8f973fed740baffc0d600
Dane <danemunday@gmail.com>
2022-03-26 18:48:24 -0700

    Added Movement to Robot

--- Assets/Scripts/RobotGuide.cs
+++ Assets/Scripts/RobotGuide.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RobotGuide : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 targetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //update player position
+        targetPosition = player.transform.position;
+        targetPosition.x -= 15;
+        targetPosition.z -= 10;
+
+        //move towards player
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 12.0f);
     }
 }
